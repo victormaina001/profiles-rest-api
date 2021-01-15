880adec89ae8877cd7c737cd4e3ed9a41f792e08
victormaina001 <ngaruiyamaina001@gmail.com>
2021-01-15 15:35:14 +0300

    added model for storing profile items,created migration and added django admin

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
 from . import models
+
+# Register your models here.
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
